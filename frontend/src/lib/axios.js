6ffe49fdf3fa9f4062a818eb4a1680ca33a8026e
Subodh Kumar Yadav <subodhkumaryadav@Subodhs-MacBook-Air.local>
2026-03-10 22:49:23 +0530

    connect frontend to production backend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : "https://mern-chat-app-x0nm.onrender.com/api",
   withCredentials: true,
 });
