3e80091c48a6a629f7ea63828fcb43d6e9dc3a87
Subodh Kumar Yadav <subodhkumaryadav@Subodhs-MacBook-Air.local>
2026-03-10 22:45:24 +0530

    fix backend app import

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
+
+import { connectDB } from "./lib/db.js";
+import { app, server } from "./lib/socket.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://mern-chat-app.vercel.app"
+    ],
     credentials: true,
   })
 );
@@ -14,3 +34,8 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
